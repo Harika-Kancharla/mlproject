1ce85366b2cb32d11d4868f7567f3079124652fa
Harika Kancharla <harika.kancharla2811@gmail.com>
2025-07-29 12:56:04 +0530

    Logging and Exception

---
+++ app.py
@@ -0,0 +1,15 @@
+from mlproject.logger import logging
+from mlproject.exception import CustomException
+import sys
+
+if __name__=="__main__":
+    logging.info("The execution has started ")
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
+
+
+
