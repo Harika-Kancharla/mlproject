f490f548927b2ab0cb5a54cdbf886da9ebe98f29
Harika Kancharla <harika.kancharla2811@gmail.com>
2025-08-28 18:49:06 +0530

    data transformation 2

--- src/mlproject/components/data_transformation.py
+++ src/mlproject/components/data_transformation.py
@@ -99,6 +99,8 @@ class DataTransformation:
             train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
+            #fit_transform() and transform() from scikit-learn always return a NumPy array
+
             logging.info(f"Saved preprocessing object")
 
             save_object(
